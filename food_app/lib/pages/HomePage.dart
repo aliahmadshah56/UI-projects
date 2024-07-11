014a53f5d5c4efd8ba95ba0a7ff17705581b4847
aliahmadshah56 <www.a7726867@gmail.com>
2024-07-11 06:13:42 +0500

    Customized AppBar

---
+++ food_app/lib/pages/HomePage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/Widgets/AppBarWidget.dart';
+
+
+class Homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          //Customized AppBar
+          Appbarwidget(),
+
+        ],
+      ),
+    );
+  }
+}
