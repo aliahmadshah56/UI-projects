b526832f887e5a42e28239a50d9c548bb4c8d417
aliahmadshah56 <www.a7726867@gmail.com>
2024-07-14 23:01:28 +0500

    Output

--- Complete_Dice_App/lib/main.dart
+++ Complete_Dice_App/lib/main.dart
@@ -8,6 +8,7 @@ class DiceApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
